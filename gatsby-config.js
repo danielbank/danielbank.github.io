4094788c74be40dd6159e8d9b08520b41bc603b1
Daniel Bank <daniel.p.bank@gmail.com>
2019-12-19 20:28:15 -0700

    Minimal Blog theme, not sure if I like it

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,59 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require(`dotenv`).config({
+  path: `.env`,
+})
 
 module.exports = {
   siteMetadata: {
-    title: "The Boogar Chronicles",
-    description: "Thoughts and Musings by Daniel Bank",
+    siteTitleAlt: `The Boogar Chronicles`,
+    navigation: [
+      {
+        title: `Blog`,
+        slug: `/blog`,
+      },
+      {
+        title: `About`,
+        slug: `/about`,
+      },
+    ],
   },
   plugins: [
-    `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `The Boogar Chronicles`,
+        short_name: `boogar-chronicles`,
+        description: `Thoughts and Musings by Daniel Bank.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
   ],
 }
